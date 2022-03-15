d3566cafe7c1977f6b92c687a3a9224f542212b1
IgorTargino <igortargino01@gmail.com>
2022-03-14 23:41:02 -0300

    feat: add route get todos

--- src/index.js
+++ src/index.js
@@ -55,7 +55,9 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  const { username } = request.header;
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
