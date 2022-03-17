67505d292557b35f40188194a0fb271e903af28a
IgorTargino <igortargino01@gmail.com>
2022-03-16 22:41:00 -0300

    refactor: return status code 404 when update todo and todo not exists

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   const todo = user.todos.find((todo) => todo.id === id);
 
   if (!todo) {
-    return response.status(400).json({
+    return response.status(404).json({
       error: "Todo not found",
     });
   }
